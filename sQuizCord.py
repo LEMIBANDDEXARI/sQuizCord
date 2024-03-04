8e17a0ee94face182fefc180d34092a68f9c827e
LEMIBANDDEXARI <70129787+LEMIBANDDEXARI@users.noreply.github.com>
2024-03-05 00:19:18 +0100

    0.2.1

--- sQuizCord.py
+++ sQuizCord.py
@@ -3,6 +3,7 @@ import interactions
 import asyncio
 import json
 import random
+import discord
 from interactions import slash_command, SlashContext
 from discord.ext import commands
 
@@ -11,17 +12,38 @@ bot = interactions.Client()
 # Liste des utilisateurs qui ont rejoint la partie
 players = []
 
+async def check_players():
+    global game_in_progress
+    while True:
+        await asyncio.sleep(1)  # Attendre une seconde
+        if game_in_progress and not players:
+            game_in_progress = False
+            if game_channel:  # Vérifier si le canal de jeu est défini
+                break  # Arrêter la boucle si la partie est terminée
+
 @interactions.listen()
 async def on_startup():
     print("Le bot est prêt !")
+    # Lancer la tâche de vérification des joueurs après la définition de votre bot
+    asyncio.create_task(check_players())
+    print(f'Je suis connecté en tant que {bot.user}')
+    servers = list(bot.guilds)
+    print(f"Connecté sur {len(servers)} serveurs:")
+    for server in servers:
+        print(f"- {server.name}")
 
 @slash_command(
     name="rejoindre",
     description="Pour rejoindre une partie.",
 )
 async def join(ctx: SlashContext):
+    global player_role
     if ctx.author not in players:
         players.append(ctx.author)
+        # Ajouter le rôle "en jeu" à l'utilisateur qui rejoint la partie
+        player_role = discord.utils.get(ctx.guild.roles, name="en jeu")
+        if player_role:
+            await ctx.author.add_roles([player_role])
         await ctx.send(f"{ctx.author.username} a rejoint la partie.")  # Utilisez 'username' au lieu de 'name'
     else:
         await ctx.send("Vous avez déjà rejoint la partie.")
@@ -31,14 +53,23 @@ async def join(ctx: SlashContext):
     description="Pour quitter une partie.",
 )
 async def leave(ctx: SlashContext):
+    global game_in_progress, player_role
     if ctx.author in players:
         players.remove(ctx.author)
+        # Retirer le rôle "joueur" de l'utilisateur qui quitte la partie
+        if player_role:
+            await ctx.author.remove_roles([player_role])
         await ctx.send(f"{ctx.author.username} a quitté la partie.")  # Utilisez 'username' au lieu de 'name'
+        # Si tous les joueurs ont quitté la partie, réinitialiser game_in_progress à False
+        if not players:
+            game_in_progress = False
+            await ctx.send("La partie a été fermée car tous les joueurs ont quitté la partie.")
     else:
         await ctx.send("Vous n'avez pas rejoint la partie.")
 
-# Ajouter une variable pour stocker l'utilisateur qui a lancé la partie
-game_starter = None
+# Variable globale pour stocker le canal de jeu
+game_channel = None
+player_role = None  # Ajouter une variable pour stocker le rôle "en jeu"
 # Ajouter une variable pour suivre si une partie est en cours
 game_in_progress = False
 # Ajouter une variable pour suivre si le jeu doit être arrêté
@@ -46,18 +77,44 @@ game_should_end = False
 class GameEndException(Exception):# Ajouter une exception personnalisée pour arrêter la partie
     pass
 
+game_master_role = None  # Ajouter une variable pour stocker le rôle "game-master"
+
+@slash_command(
+    name="game-master",
+    description="Pour devenir ou cesser d'être le game-master.",
+)
+async def game_master(ctx: SlashContext):
+    global game_master_role
+    game_master_role = discord.utils.get(ctx.guild.roles, name="game-master")
+    if game_master_role in ctx.author.roles:
+        await ctx.author.remove_roles([game_master_role])
+        await ctx.send(f"{ctx.author.username} n'est plus le game-master.")
+    else:
+        if any(game_master_role in member.roles for member in ctx.guild.members):
+            await ctx.send("Il y a déjà un game-master.")
+        else:
+            await ctx.author.add_roles([game_master_role])
+            await ctx.send(f"{ctx.author.username} est maintenant le game-master.")
+
 @slash_command(
     name="start",
     description="Pour commencer une partie.",
 )
 async def start(ctx: SlashContext):
-    global game_starter, game_in_progress, game_should_end
+    global game_starter, game_in_progress, game_should_end, game_ended_by_starter, game_channel, game_master_role
+    game_master_role = discord.utils.get(ctx.guild.roles, name="game-master")
+    if game_master_role not in ctx.author.roles:
+        await ctx.send("Seul le game-master peut commencer une partie.")
+        return
+    game_channel = ctx.channel  # Stocker le canal de jeu
     game_should_end = False  # Réinitialiser l'état d'arrêt du jeu
-    if game_in_progress:
+    if game_in_progress and not game_ended_by_starter:
         await ctx.send("Une partie est déjà en cours.")
         return
     game_starter = ctx.author
     game_in_progress = True
+    # Ajouter ici la réinitialisation de game_ended_by_starter
+    game_ended_by_starter = False
     if not players:
         await ctx.send("Aucun joueur n'a rejoint la partie.")
         return
@@ -72,6 +129,10 @@ async def start(ctx: SlashContext):
     message = await ctx.send("La partie va commencer dans 20 secondes.")
     for i in range(19, 0, -1):
         await asyncio.sleep(1)
+        if game_should_end or not players:  # Vérifier si le jeu doit être arrêté ou si tous les joueurs ont quitté
+            await ctx.send("La partie a été arrêté.")
+            game_in_progress = False
+            return
         if i == 1:
             await message.edit(content="La partie commence.")
         else:
@@ -124,9 +185,11 @@ async def start(ctx: SlashContext):
             await ctx.send(f"Temps écoulé\nLa réponse était : {correct_answer}")
             await asyncio.sleep(15)  # Ajout d'un décompte de 15 secondes
     except GameEndException:
+        game_in_progress = False  # Réinitialiser l'état du jeu ici
         await ctx.send("La partie a été interrompue.")
         return
 
+    game_in_progress = False  # Réinitialiser l'état du jeu ici aussi, si le jeu se termine normalement
     await ctx.send("Fin de la partie.")  # Afficher "fin de la partie" à la fin
 
 @slash_command(
@@ -134,15 +197,17 @@ async def start(ctx: SlashContext):
     description="Pour mettre fin à une partie.",
 )
 async def end(ctx: SlashContext):
-    global game_starter, game_in_progress, game_should_end
-    if game_in_progress:  # Vérifier si une partie est en cours
-        if ctx.author == game_starter:  # Vérifier si l'utilisateur qui tente de mettre fin à la partie est celui qui l'a commencée
+    global game_starter, game_in_progress, game_should_end, game_ended_by_starter
+    if game_in_progress and game_starter is not None:  # Vérifier si une partie est en cours et si game_starter n'est pas None
+        if ctx.author.id == game_starter.id:  # Vérifier si l'utilisateur qui tente de mettre fin à la partie est celui qui l'a commencée
             game_starter = None  # Réinitialiser l'utilisateur qui a lancé la partie
-            game_in_progress = False  # Indiquer qu'aucune partie n'est en cours
             game_should_end = True  # Indiquer que le jeu doit être arrêté
+            game_ended_by_starter = True  # Indiquer que la partie a été terminée par l'utilisateur qui l'a lancée
+            game_in_progress = False  # Réinitialiser l'état du jeu ici
         else:
             await ctx.send("Seul l'utilisateur qui a lancé la partie peut la terminer.")
-    else:
+            return  # Ajouter un return ici pour arrêter l'exécution de la fonction si l'utilisateur n'est pas celui qui a commencé la partie
+    elif not game_in_progress or (game_starter is None and not game_ended_by_starter):  # Ajout de cette condition pour vérifier si aucune partie n'est en cours ou si game_starter est None et la partie n'a pas été terminée par l'utilisateur qui l'a lancée
         await ctx.send("Aucune partie n'est en cours.")  # Indiquer qu'aucune partie n'est en cours si l'utilisateur tente de mettre fin à une partie qui n'a pas commencé
 
 bot.start(os.getenv('DISCORD_TOKEN'))
